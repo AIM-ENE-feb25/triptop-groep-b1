3115aa781c4ec8b2660e382f0001b7a3aff3a4d6
MeneerJohnson <150233363+MeneerJohnson@users.noreply.github.com>
2025-03-25 12:45:02 +0100

    huiswerk week 2 les 2

---
+++ Les-Opdrachten/Week-2/Les-2/example.js
@@ -0,0 +1,16 @@
+
+//Niet goed -> Deze functie regelt processing en save
+function multiplyAndSave(number){
+    // Processing data
+    var newNumber = number * 2;
+    // Saving data
+    println(newNumber); //Dit zou dan een save functie moeten zijn
+}
+
+//Wel goed -> hierbij heeft elke functie 1 verantwoordelijkheid
+function multiply(number){
+    return number * 2;
+}
+function save(number){
+    println(number) //Dit zou dan een save functie moeten zijn
+}
