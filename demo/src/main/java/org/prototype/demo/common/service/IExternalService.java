3dc1a4e4891c210d18104a6a65948b38230e5692
noahvolman <N.Volman@student.han.nl>
2025-04-03 16:32:41 +0200

    alles weg halen
    ik begin overnieuw aan de hand van de aangepaste informatie in de main.

--- demo/src/main/java/org/prototype/demo/common/service/IExternalService.java
+++
@@ -1,9 +0,0 @@
-package org.prototype.demo.common.service;
-
-import org.prototype.demo.common.model.ApiResponse;
-import java.util.concurrent.CompletableFuture;
-
-public interface IExternalService {
-    CompletableFuture<ApiResponse> executeRequest(Object request);
-    boolean validate(ApiResponse response);
-} 
